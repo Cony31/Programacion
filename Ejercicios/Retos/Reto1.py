b7590ea5d13b2521fec56655d21f83eed4a7e546
Cony31 <128840877+Cony31@users.noreply.github.com>
2023-06-02 15:44:40 -0400

    Create Reto1.py

---
+++ Ejercicios/Retos/Reto1.py
@@ -0,0 +1,14 @@
+nombre = input("¿Cual es tu nombre?\n")
+edad = input("¿Cual es tu edad?\n")
+print(type(edad))
+totaledad = int(edad) + 20
+
+
+print(
+    "Tu nombre es",
+    nombre,
+    ", tu edad es",
+    edad,
+    "y tu edad total en 20 años es",
+    totaledad,
+)
