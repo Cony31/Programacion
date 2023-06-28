241cdebe04d1c867491ba420f825a2a4b6ea32c9
Cony31 <128840877+Cony31@users.noreply.github.com>
2023-06-28 15:16:46 -0400

    Update Bucles.py

--- Material/Bucles.py
+++ Material/Bucles.py
@@ -1,7 +1,4 @@
-from colorama import init, Fore
-init(autoreset=True)
-
-#01-WHILE
+#WHILE
 edad = 15
 num = 0
 
@@ -15,52 +12,52 @@ num = 0
     num +=2"""
 
 #¿Qué hace este bucle?
-print(Fore.GREEN + "01 - WHILE")
-print(Fore.YELLOW +'\nImpresión de numeros de 0 al 100 (Incrementando de 2 en 2)')
+print("01 - WHILE")
+print('\nImpresión de numeros de 0 al 100 (Incrementando de 2 en 2)')
 
 num = 0
 while num <= 100:
     print(num)
     num += 2
     #num = num + 2
-print(Fore.RED + "Primer Bucle terminado!\n")
+print("Primer Bucle terminado!\n")
 
 #Combinando While y else
-print(Fore.YELLOW +'\nImpresión de numeros de 100 al 200 (Incrementando de 2 en 2) + Condicion ELSE')
+print('\nImpresión de numeros de 100 al 200 (Incrementando de 2 en 2) + Condicion ELSE')
 
 while num <= 200:
     print(num)
     num += 2
 else: 
     print("Mi condicion es igual o mayor a 200")
-print(Fore.RED + "Segundo Bucle terminado!")
+print("Segundo Bucle terminado!")
 
 
 #Combinando While y if
-print(Fore.YELLOW +'\nImpresión de numeros de 200 al 300 (Incrementando de 2 en 2) + IF en 250')
+print('\nImpresión de numeros de 200 al 300 (Incrementando de 2 en 2) + IF en 250')
 
 while num <= 300:
     print(num)
     num += 2
     if num == 250:  #Mover if izquierda
         print("Mi condicion es igual a 250")
-print(Fore.RED + "Tercer Bucle terminado!\n")
+print("Tercer Bucle terminado!\n")
 
 #Utilizando el break
-print(Fore.YELLOW +'\nImpresión de numeros de 300 al 400 (Incrementando de 2 en 2) + Break a los 350')
+print('\nImpresión de numeros de 300 al 400 (Incrementando de 2 en 2) + Break a los 350')
 
 while num <= 400:
     print(num)
     num += 2
     if num == 350:  #mover if izquierda
-        print(Fore.MAGENTA + "Se detiene la ejecución")
+        print("Se detiene la ejecución")
         break
 print(num)
-print(Fore.RED + "Cuarto Bucle terminado!\n")
+print("Cuarto Bucle terminado!\n")
 
 
 #Utilizando el continue
-print(Fore.YELLOW +'\nImpresión de numeros de 0 al 50 (Incrementando de 1 en 1) + continue if == 40')
+print('\nImpresión de numeros de 0 al 50 (Incrementando de 1 en 1) + continue if == 40')
 num = 0
 while num <= 50:
     num += 1
@@ -77,27 +74,27 @@ while num <= 50:
         print(parametro)"""
 
 #Bucle For
-print(Fore.GREEN + "02 - FOR")
+print("02 - FOR")
 
 
-print(Fore.YELLOW +'\nImpresión de una lista de 10 elementos de forma vertical (Utilizando FOR)')
-print(Fore.YELLOW +'\nFOR N°1')
+print('\nImpresión de una lista de 10 elementos de forma vertical (Utilizando FOR)')
+print('\nFOR N°1')
 
 #No esta bien optimizado
 for i in (1,2,3,4,5,6,7,8,9,10):
     print(i)
 
-print(Fore.YELLOW +'\nFOR N°2')
+print('\nFOR N°2')
 newlista = [1,2,3,4,5,6,7,8,9,10]
 for i in newlista:
     print(i)
 
-print(Fore.YELLOW +'\nFOR N°3')
+print('\nFOR N°3')
 for i in range(1,11):
     print(i)
 
 
-print(Fore.GREEN + "\n03 - ITERADOR E ITERABLES")
+print("\n03 - ITERADOR E ITERABLES")
 #Aprendiendo los conceptos de iterador e iterable
 iterador = iter(newlista)
 
