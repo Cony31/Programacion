fd35d0808143fd81b5dacfdf3e417ef8b9a17b5b
Cony31 <128840877+Cony31@users.noreply.github.com>
2023-06-28 14:54:08 -0400

    Update Funciones.py

--- Material/Funciones.py
+++ Material/Funciones.py
@@ -1,58 +1,80 @@
-# Declarando la funciona en Python
-
+#01-DECLARANDO LA PRIMERA FUNCIÓN
+print("DECLARANDO UNA FUNCIÓN SIMPLE")
 
 def La_primera_funcion():
     print("Esta es mi primera funcion")
 
-
 La_primera_funcion()
 
-# Declarando una funcion y utilizando listas
 
+#02-DECLARANDO UNA FUNCIÓN Y UTILIZANDO LISTAS
+print("\nDECLARANDO UNA FUNCIÓN Y UTILIZANDO LISTAS")
 
-def Concatenar(lista1, lista2):
+def concatenar(lista1,lista2):
     return lista1 + lista2
 
+lista1 = [1,2,3]
+lista2 = [4,5,6]
 
-lista1 = [1, 2, 3]
-lista2 = [4, 5, 6]
-
-Concatenar()
-print(Concatenar(lista1, lista2))
-
-# Declarando una funcion de multiplicacion
+#concatenar()
+print(concatenar(lista1,lista2))
 
 
-def multiplicacion(num1, num2):
-    return num1 + num2
+#03-DECLARANDO UNA FUNCIÓN MULTIPLICACION PASANDO PARAMETROS
+print("\nDECLARANDO UNA FUNCIÓN MULTIPLICACION PASANDO PARAMETROS")
+
+def multiplicacion(num1,num2):
+    return num1*num2
+
+#multiplicacion()
+print(multiplicacion(50,50))
 
 
-multiplicacion()
-print(multiplicacion(10, 2))
-
-# ejemplo de una funcion suma y resta
-
+#04-EJEMPLO DE UNA FUNCIÓN
+print("\nFUNCIONES SUMA Y RESTA (POR TECLADO)")
 
 def suma(a, b):
     return a + b
 
-
 def resta(a, b):
     return a - b
 
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
 
-# se llama a la funcion de suma
+#Se llama a la función Suma
 resultado = suma(a, b)
-print("la suma es de:", resultado)
+print("La suma es de:", resultado)
 
-# se llama a la funcion de resta
+#Se llama a la función resta
 resultado2 = resta(a, b)
-print("la resta es de:", resultado2)
+print("La resta es de:", resultado2)
 
 
-def saludo(nombre):
-    print(f"Hola, mi nombre es + {nombre}")
+#05-PASANDO PARAMETROS POR VALOR
+print("\n05-PASANDO PARÁMETROS POR VALOR")
+def modificar_numero(x):
+    x = 10  
+
+x = 5
+print("Antes de llamar a la función:", x)  
+modificar_numero(x)
+print("Después de llamar a la función:", x)  
 
 
-saludo("Tomas")
-# Saludo: la llamada a la funcion, imprime "Hola, mi nombre es Tomas"
+#06-PASANDO PARAMETROS POR REFERENCIA
+print("\n06-PASANDO PARÁMETROS POR REFERENCIA")
+
+#Paso por referencia (objetos mutables)
+def op_lista(lista):
+    lista.append(4)  #Modificando la lista original
+
+numeros = [1, 2, 3]
+op_lista(numeros)  
+print(numeros)  
+
+#Paso por nombre de argumento
+def saludar(nombre, mensaje):
+    print(mensaje + " " + nombre)
+
+saludar(mensaje="Hola,", nombre="Juan")  
